a67e95881e5e85ea022477c5dfc6c03832d0be54
Michael Long <130015630+ClankDev@users.noreply.github.com>
2023-08-20 23:14:25 -0400

    Update RtfConverter.php

--- src/php/RtfConverter.php
+++ src/php/RtfConverter.php
@@ -3,6 +3,13 @@ namespace RtfConverter;
 
 class RtfConverter {
     public static function rtfToTxt($rtf) {
+        if ($rtf === null) {
+            return '';
+        }
+        
+        // Directly remove patterns like 'listtemplateid' before processing
+        $rtf = preg_replace('/\\\\listtemplateid[^\s}]+/', '', $rtf);
+        
         $out = '';
         $len = strlen($rtf);
         $ignorable = false;
@@ -10,6 +17,7 @@ class RtfConverter {
 
         for ($i = 0; $i < $len; $i++) {
             $c = $rtf[$i];
+
             switch ($c) {
                 case '{':
                     if (!$inColorTable) {
@@ -21,7 +29,6 @@ class RtfConverter {
                     break;
                 case '\\':
                     if ($ignorable) {
-                        // Skip if inside ignored block
                         if ($i + 1 < $len && ctype_alpha($rtf[$i + 1])) {
                             $i++; // Skip the control word
                         }
@@ -29,33 +36,31 @@ class RtfConverter {
                             // Skip hex code
                             $i += 2; // Skip the \'
                         }
-                        continue 2; // Use "continue 2" to skip the current loop iteration
+                        continue 2; 
                     }
 
                     $word = '';
                     $i++;
 
-                    while ($i < $len && ctype_alpha($rtf[$i])) {
+                    while ($i < $len && (ctype_alpha($rtf[$i]) || ctype_digit($rtf[$i]) || $rtf[$i] === '-')) {
                         $word .= $rtf[$i];
                         $i++;
                     }
 
                     if ($word === 'colortbl') {
                         $inColorTable = true;
-                        // Skip color table content until semicolon is encountered
                         while ($i < $len && $rtf[$i] !== ';') {
                             $i++;
                         }
                         $inColorTable = false;
-                    } elseif ($word === 'u') {
-                        // Handle Unicode character
-                        $param = '';
-                        while ($i < $len && ctype_digit($rtf[$i])) {
-                            $param .= $rtf[$i];
-                            $i++;
-                        }
-                        $out .= mb_convert_encoding("&#" . intval($param) . ";", 'UTF-8', 'HTML-ENTITIES');
+                    } elseif ($word === 'u8364') {  // Directly check for the Euro Unicode character
+                        $out .= '€';
                         if ($i < $len && $rtf[$i] == '?') $i++;
+                    } elseif ($word[0] === 'u' && is_numeric(substr($word, 1))) {
+                        $out .= mb_convert_encoding("&#" . intval(substr($word, 1)) . ";", 'UTF-8', 'HTML-ENTITIES');
+                        if ($i < $len && $rtf[$i] == '?') $i++;
+                    } elseif (in_array($word, ['b', 'i', 'list', 'listlevel', 'listhybrid'])) {
+                        continue 2;
                     }
                     break;
                 default:
@@ -68,5 +73,4 @@ class RtfConverter {
         return $out;
     }
 }
-
 ?>
